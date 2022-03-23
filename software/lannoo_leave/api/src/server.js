a77726233a8189eb3c0da54075a61e3321838ce8
arno <arno.schoutteten@student.vives.be>
2022-03-23 13:58:49 +0100

    node routes added, also joey changes some things

--- software/lannoo_leave/api/src/server.js
+++ software/lannoo_leave/api/src/server.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+var jsonParser = bodyParser.json()
 
 app.get('/', (req, res) => {
   res.send('Welcome to API for Lannoo leaf')
+})
+
+app.get('/import', (req, res) => {
+  res.send('Welcome to API for Lannoo leaf')
+})
+
+app.post('/', jsonParser, function (req, res) {
+  console.log(JSON.stringify(req.body))
+  res.send('Your data: ' + JSON.stringify(req.body))
 })
 
 app.listen(port, () => {

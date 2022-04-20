d16563c9efb038d7a49c787b3bba1afa3c4b3253
arno <arno.schoutteten@student.vives.be>
2022-04-20 15:13:23 +0200

    it works :)

--- software/LannooLeaf/api/src/server.js
+++ software/LannooLeaf/api/src/server.js
@@ -1,30 +1,56 @@
 import express from 'express';
-// import { Lannooleaf, Color, Graph } from 'lannooleaf_spi_controller';
+import { Lannooleaf, Color, Graph } from 'lannooleaf_spi_controller';
 import bodyParser from 'body-parser';
-import cors from 'cors'
-const app = express()
-const port = 3000
+import cors from 'cors';
+const controller = new Lannooleaf();
+controller.Init()
+.then(() => {
+  const app = express();
+  const port = 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
-var jsonParser = bodyParser.json()
 
-app.get('/', (req, res) => {
-  res.send('Welcome to API for Lannoo leaf')
-})
+  app.use(cors());
+  app.use(bodyParser.json());
+  var jsonParser = bodyParser.json();
 
-app.get('/spi', (req, res) => {
-  main();
-  res.send('SPI-DEMO!')
-})
+  app.get('/', (req, res) => {
+    res.send('Welcome to API for Lannoo leaf');
+  })
 
-app.post('/', jsonParser, function (req, res) {
-  console.log(JSON.stringify(req.body))
-  // res.send('Poepsnor!')
-  res.send('Your data: ' + JSON.stringify(req.body))
-})
+  app.post('/', jsonParser, (req, res) => {
+    console.log(JSON.stringify(req.body));
+    res.send('Your data: ' + JSON.stringify(req.body));
+    if(req.body.effect) {
+      effect1();
+    }
+    if(req.body.color) {
+      let color = req.body.color.substring(1);
+      let red = Number('0x' + color.substring(0,2));
+      let green = Number('0x' + color.substring(2,4));
+      let blue = Number('0x' + color.substring(4));
+      controller.SetAll(new Color(red,green,blue));
+    }
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
 
+});
+
+async function effect1 () {
+  for (let index = 0; index < 20; index++) {
+    for (let i = 0; i < 255; i++) {
+      await controller.SetAll(new Color(i,0,0));
+      await new Promise(resolve => {setTimeout(resolve,10)})
+    }
+    for (let i = 0; i < 255; i++) {
+      await controller.SetAll(new Color(0,i,0));
+      await new Promise(resolve => {setTimeout(resolve,10)})
+    }
+    for (let i = 0; i < 255; i++) {
+      await controller.SetAll(new Color(0,0,i));
+      await new Promise(resolve => {setTimeout(resolve,10)})
+    }
+  }
+}

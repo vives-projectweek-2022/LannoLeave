c9c30457b3842b6af517cf92d77c16b2f1cfd9a1
madness007 <arno.schoutteten@gmail.com>
2022-05-03 21:45:29 +0200

    artnet api

--- software/artnet/index.js
+++ software/artnet/index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 var dmxlib=require('dmxnet');
 
 var dmxnet = new dmxlib.dmxnet({
@@ -8,12 +9,47 @@ var dmxnet = new dmxlib.dmxnet({
     hosts: ["127.0.0.1"] // Interfaces to listen to, all by default
   });
   
+var ClearToSend = true;
+
 var receiver=dmxnet.newReceiver({
     subnet: 0, //Destination subnet, default 0
     universe: 0, //Destination universe, default 0
     net: 0, //Destination net, default 0
   });
 
-  receiver.on('data', function(data) {
-    console.log('DMX data:', data);
+  receiver.on('data', async function(data) {
+    let dataArray = new Array(13);
+    let addressCounter = 0;
+    for (let i = 0; i < 13; i++) {
+      dataArray[i] = new Array(13);
+      for (let ii = 0; ii < 13; ii++) {
+        if (i%2 == 1) {
+          dataArray[i][ii] = [data[addressCounter++],data[addressCounter++],data[addressCounter++]];
+        }
+        else {
+          dataArray[i][12-ii] = [data[addressCounter++],data[addressCounter++],data[addressCounter++]];
+        }
+      }
+    }
+    // console.log(dataArray);
+    if(ClearToSend) {
+      ClearToSend = false
+      axios
+        .post('http://10.5.8.2:3000/matrix', dataArray)
+        .then(res => {
+          console.log(`statusCode: ${res.status}`);
+        })
+        .catch(error => {
+          // console.error(error);
+          console.log(`ERROR`);
+        }); 
+        await new Promise(resolve => {setTimeout(resolve, 40)})
+        ClearToSend = true;
+    }
+    // console.log(data[0]);
+    // console.log('DMX data:', data);
   });
+
+
+
+

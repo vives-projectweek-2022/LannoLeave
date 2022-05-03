df4cd6dea054c56abee767e9c11900ec01b42be4
Joey <joey@de-smet.vlaanderen>
2022-05-03 15:11:47 +0200

    stuff

--- software/websocket_transfer/Client/client.py
+++ software/websocket_transfer/Client/client.py
@@ -1,6 +1,5 @@
 import os
 import json
-import math
 import hashlib
 import requests
 import argparse
@@ -12,128 +11,33 @@ from time import sleep
 from PIL import Image
 from websocket import create_connection
 
-# Data preparation
-def findSmallest(coord_led_array):
-  smallest_x = None
-  smallest_y = None
+import preprocces as pp
+from interpolation import intrepolate
 
-  for pair in coord_led_array:
-    x = pair[0]['x']
-    y = pair[0]['y']
-
-    if (smallest_x is None or x < smallest_x):
-      smallest_x = x
-
-    if (smallest_y is None or y < smallest_y):
-      smallest_y = y
-
-  return (smallest_x, smallest_y)
-
-def findBiggest(coord_led_array):
-  biggest_x = None
-  biggest_y = None
-
-  for pair in coord_led_array:
-    x = pair[0]['x']
-    y = pair[0]['y']
-
-    if (biggest_x is None or x > biggest_x):
-      biggest_x = x
-
-    if (biggest_y is None or y > biggest_y):
-      biggest_y = y
-
-  return (biggest_x, biggest_y)
-
-def makeAllPositive(coord_led_array, increase_coor):
-  x = abs(increase_coor[0])
-  y = abs(increase_coor[1])
-
-  for pair in coord_led_array:
-    pair[0]['x'] += x
-    pair[0]['y'] += y
-
-def scalePixelsToImage(coord_led_array, image):
-  makeAllPositive(coord_led_array, findSmallest(coord_led_array))
-  biggest = findBiggest(coord_led_array)
-  x_scale = image.width / biggest[0] -  0.1
-  y_scale = image.height / biggest[1] - 0.1
-
-  for pair in coord_led_array:
-    pair[0]['x'] = math.floor(x_scale * pair[0]['x'])
-    pair[0]['y'] = math.floor(y_scale * pair[0]['y'])
-
-def flip_x_axis(coord_led_array):
-  for pair in coord_led_array:
-    pair[0]['y'] = - pair[0]['y']
-
-def prepare_coordinates(coord_led_array, image):
-  flip_x_axis(coord_led_array)
-  makeAllPositive(coord_led_array, 
-                  findSmallest(coord_led_array))
-  scalePixelsToImage(coord_led_array, image)
-
-def dist(x1,y1,x2,y2):
-  d = np.sqrt((x2-x1)**2+(y2-y1)**2)
-  return d
-
+# Parsing arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-im', '--image', type=str, required=True, help="Path to image source")
 parser.add_argument('-i', '--ip', type=str, required=True, help="Ip address of websocket server")
 parser.add_argument('-p', '--port', type=str, default=3000, help="Port of websocket server default is 3000")
+parser.add_argument('--interpolate', help="Tells script to use interpolation")
 
 args = parser.parse_args()
 
 image = Image.open(args.image)
 
+# Getting coordinates from api
 req = requests.get(f"http://{args.ip}:{args.port}/coordinates")
 json_data = req.json()
 
 shape_hash = hashlib.md5(json.dumps(json_data).encode("utf-8"))
 
-print(shape_hash.hexdigest())
-
-prepare_coordinates(json_data, image)
+pp.prepare_coordinates(json_data, image)
 
 x, y = [], []
-coords = []
 
 for i, pair in enumerate(json_data):
   x.append(pair[0]['x'])
   y.append(pair[0]['y'])
-  coords.append((pair[0]['x'], pair[0]['y']))
-
-def intrepolate(image):
-  vdist = np.vectorize(dist)
-
-  im = np.array(image)
-
-  nx, ny = im.shape[1], im.shape[0]
-  xx = np.arange(0, nx, 1)
-  yy = np.arange(0, ny, 1)
-
-  newx = x
-  newy = y
-
-  vnewx = np.transpose(np.tile(newx,[len(xx),len(yy),1]),(2,1,0))
-  vnewy = np.transpose(np.tile(newy,[len(xx),len(yy),1]),(2,1,0))
-  vxx = np.tile(xx,[len(newx),len(yy),1])
-  vyy = np.transpose(np.tile(yy,[len(newx),len(xx),1]),(0,2,1))
-  distances = vdist(vnewx,vnewy,vxx,vyy)
-
-  mind = np.argmin(distances, 0)
-
-  imr = im[:,:,0]
-  img = im[:,:,1]
-  imb = im[:,:,2]
-
-  newc = np.zeros((len(newx),3))
-  for i in range(newc.shape[0]):
-    newc[i,0] = np.mean(imr[np.where(mind==i)])
-    newc[i,1] = np.mean(img[np.where(mind==i)])
-    newc[i,2] = np.mean(imb[np.where(mind==i)])
-
-  return newc
 
 fullGif = []
 
@@ -144,7 +48,7 @@ if not os.path.exists(f"./saves/{shape_hash.hexdigest()}/{base}.npy"):
   for frame in range(image.n_frames):
     print(f"Proccessing frame {frame} of {image.n_frames}")
     image.seek(frame)
-    fullGif.append(intrepolate(image.convert("RGB")))
+    fullGif.append(intrepolate(image.convert("RGB"), x, y))
 
   print("Saveing proccessed images")
 
@@ -178,5 +82,9 @@ while True:
       i+=1
 
     ws.send(json.dumps(m_data))
+    run = ws.recv()
+    
+    if (not bool(run)): break
+
     ws.close()
     sleep(0.033)
--- software/websocket_transfer/Server/index.js
+++ software/websocket_transfer/Server/index.js
@@ -1,51 +1,76 @@
-import express, { json } from 'express';
 import http from 'http';
+import cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+
 import { WebSocketServer } from 'ws';
 import { Color, Lannooleaf, Graph, CoordMap } from 'lannooleaf_spi_controller';
 
-const controller = new Lannooleaf();
 const graph = new Graph();
+const controller = new Lannooleaf();
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server: server });
 
-wss.on('connection', async ws => {
+var run = true;
 
-  // Listening on websocket for message
+app.use(cors());
+app.use(bodyParser.json());
+
+/*------------------------------------- GetNPost stuff -------------------------------------*/
+
+app.get('/', (req, res) => {
+  res.send('Welcome to API for Lannoo leaf');
+});
+
+app.get("/count", (req, res) => {
+  res.send({
+    size: graph.map.size()
+  });
+});
+
+app.get("/coordinates", async (req, res) => {
+  const coorMap = new CoordMap(graph);
+  res.send(coorMap.coordToJson());
+});
+
+app.post('/', jsonParser, async (req, res) => {
+  console.log(JSON.stringify(req.body));
+  
+  if(req.body.color) {
+    let color = req.body.color.substring(1);
+
+    let red = Number('0x' + color.substring(0,2));
+    let green = Number('0x' + color.substring(2,4));
+    let blue = Number('0x' + color.substring(4));
+
+    await controller.SetAll(new Color(red, green, blue));
+  }
+
+  res.send('Your data: ' + JSON.stringify(req.body));
+});
+
+/*------------------------------------- Websocket stuff -------------------------------------*/
+
+wss.on('connection', async ws => {
   ws.on('message', async message => {
     let json_message = JSON.parse(message);
 
     for (var address in json_message) {
       let colorString = [];
 
-      // Contructing led string from received json data
-      json_message[address].forEach(color => {
-        colorString.push(new Color(color[0], color[1], color[2]));
-      });
+      json_message[address].forEach(color => { colorString.push(new Color(color[0], color[1], color[2])); });
       
-      // Sending ledstring to lannooleaf on error just log
-      try {
-        await controller.SetLedString(parseInt(address), colorString);
-      } catch (error) {
-        console.error(error)
-      }
+      await controller.SetLedString(parseInt(address), colorString);
+      
+      ws.send(run);
     }
-
   });
-
 });
 
-// Get request for python script to get coordinates
-app.get("/coordinates", async (req, res) => {
-  const coorMap = new CoordMap(graph);
-  res.send(coorMap.coordToJson());
-});
-
-// start our server
 server.listen(3001, async () => {
   await controller.Init();
-  // Getting graph from lannooleaf
   await controller.GetGraph(graph);
   console.log(`Server started on port ${(server.address()).port} :)`);
 });

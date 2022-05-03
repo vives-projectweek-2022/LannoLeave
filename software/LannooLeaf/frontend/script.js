a9d95b09029eb6e6d2b0d0f465466e8cc23cce11
Jay D'Hulster <jaydhulster@gmail.com>
2022-05-03 14:16:28 +0200

    added unit

--- software/LannooLeaf/frontend/script.js
+++ software/LannooLeaf/frontend/script.js
@@ -1,7 +1,7 @@
 var backend = "http://172.16.240.30:3000"
 
 $("#RGB").on('propertychange input', function(){
-    var JsonData = JSON.stringify({"mode": "color", "leaf": $('#select_leaf').val() , "led": $('#select_led').val() ,"color": $(this).val()});
+    var JsonData = JSON.stringify({"mode": "color", "unit": $('#select_unit').val(), "leaf": $('#select_leaf').val() , "led": $('#select_led').val() ,"color": $(this).val()});
     post(JsonData, ""); 
   });
 
@@ -33,26 +33,35 @@ function post(data, url) {
     xhr.send(data);
     
 }
-
+let dropdownunit = $('#select_unit');
 let dropdownleaf = $('#select_leaf');
 let dropdownled = $('#select_led');
 
+dropdownunit.empty();
 dropdownleaf.empty();
 dropdownled.empty();
 
+dropdownunit.append('<option selected="true">all</option>');
+dropdownunit.prop('selectedIndex', 0)
 dropdownleaf.append('<option selected="true">all</option>');
 dropdownleaf.prop('selectedIndex', 0)
 dropdownled.append('<option selected="true">all</option>');
 dropdownled.prop('selectedIndex', 0)
 
-var leaves = 5;
- // Count leaves
-    for(let i = 0; i < leaves; i++) {
+var units = 5;
+ // Count units
+    for(let i = 0; i < units; i++) {
+        dropdownunit.append('<option>' + i + '</option>')
+    }
+
+    // leaf list
+    for(let i = 0; i < 8; i++) {
         dropdownleaf.append('<option>' + i + '</option>')
     }
 
+
 // Led list
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < 2; i++) {
         dropdownled.append('<option>' + i + '</option>')
     }
 
